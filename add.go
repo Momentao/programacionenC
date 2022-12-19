843f743bc56cee64a6c22eb51c9c183886bc56eb
Momentao <91534139+Momentao@users.noreply.github.com>
2022-12-19 22:53:16 +0000

    Fixing paths

--- add.go
+++ add.go
@@ -8,3 +8,33 @@ import (
 )
 
 //func (player *Player) Add(args []string) (resp, err string) {
+//	if len(args) == 0 {
+//		err = ("nothing specified, nothing added")
+//		return
+//	}
+//
+//	found := 0
+//	notfound := 0
+//	errstr := "not found:"
+//	for _, arg := range args {
+//		items := player.Find(arg)
+//
+//		// TODO: item may be directory, add recursively
+//		if len(items) == 0 {
+//			errstr += " " + arg
+//			notfound++
+//		}
+//
+//		player.playlist.Append(items...)
+//		found += len(items)
+//
+//	}
+//
+//	if notfound > 0 {
+//		err = errstr
+//	}
+//	if found > 0 {
+//		resp = fmt.Sprint("added ", found, " files to playlist")
+//	}
+//	return
+//}
---
+++ api.go
@@ -0,0 +1,8 @@
+
+package main
+
+// The API wraps a player and only exports
+// methods meant to be called by the user.
+type API struct {
+	player *Player
+}
