aa7d4dfca83663fdfe6728e9a9428dc17eb19b98
Momentao <91534139+Momentao@users.noreply.github.com>
2022-12-23 12:48:30 +0000

    test?

--- client.go
+++ client.go
@@ -1,4 +1,5 @@
 package main
 
 // This file implements the client main function.
-// Invoked whenever the user t
+// Invoked whenever the user types executes "clip".
+// The client merely forwards the CLI argument
