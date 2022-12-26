fda69aba685c61bae118d9eb819f6e5e60f65e45
Momentao <91534139+Momentao@users.noreply.github.com>
2022-12-26 05:56:36 +0000

    test2

--- client.go
+++ client.go
@@ -4,4 +4,12 @@ package main
 // Invoked whenever the user types executes "clip".
 // The client merely forwards the CLI arguments
 // to the clip daemon and returns the response to
-// the use
+// the user.
+
+import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"os/exec"
+	"strings"
+	
