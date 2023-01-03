78b55d70bec4747d853909236d68df29323feea5
Momentao <91534139+Momentao@users.noreply.github.com>
2023-01-03 14:17:28 +0000

    New translations default.json (Dutch)

--- debug.go
+++ debug.go
@@ -10,3 +10,19 @@ import (
 
 // Print debug message to stderr.
 func Debug(msg ...interface{}) {
+	fmt.Fprintln(os.Stderr, msg...)
+}
+
+// Print error message and exit.
+func Err(msg ...interface{}) {
+	fmt.Fprintln(os.Stderr, msg...)
+	os.Exit(3)
+}
+
+// Check for error.
+// If err != nil, print it and exit.
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
