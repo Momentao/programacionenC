610618bfd0ee41aebceaab28c1eeaffe6103dbd2
Momentao <91534139+Momentao@users.noreply.github.com>
2023-01-15 16:43:06 +0000

    add test framework

--- library.go
+++ library.go
@@ -76,3 +76,50 @@ func (lib *Lib) ImportFile(file string) {
 		lib.AddTag(title)
 	}
 	title.file = file
+}
+
+func (lib *Lib) AddTag(tag *Tag) {
+	lib.tags = append(lib.tags, tag)
+}
+
+// TODO: possibly return multiple tags. 
+// TODO: do not return tag that is already in parent tag (like song in album with same name)
+func (lib *Lib) FindTag(tag string) *Tag {
+	for _, t := range lib.tags {
+		if t.fuzzy == tag {
+			return t
+		}
+	}
+	return nil
+}
+
+// Print the entire library recursively
+func (lib *Lib) WriteTo(out io.Writer) (n int, err error) {
+	for _, clip := range lib.clips {
+		N, ERR := fmt.Fprintln(out, clip)
+		if ERR != nil {
+			err = ERR
+		}
+		n += N
+	}
+	return
+}
+
+func (lib *Lib) String() string {
+	buf := bytes.NewBuffer([]byte{})
+	lib.WriteTo(buf)
+	return string(buf.Bytes())
+}
+
+// Find items based on tag
+//func (lib *Lib) Find(tag string) (items []*Item) {
+//	tag = Fuzzy(tag)
+//	clips = []*Item{}
+//	for _, clip := range lib.clips {
+//		if clip.tag == tag {
+//			items = append(items, item)
+//			Debug("find", tag, ":", item.file)
+//		}
+//	}
+//	return
+//}
---
+++ ls.go
@@ -0,0 +1,15 @@
+
+package main
+
+// This file implements the "ls" command.
+
+import (
+//"bytes"
+)
+
+//func (player *Player)Ls(args []string) (resp string, err os.Error) {
+//	buf := bytes.NewBuffer([]byte{})
+//	player.library.WriteTo(buf)
+//	resp = buf.String()
+//	return
+//}
