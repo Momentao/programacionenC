bd013d3090efdc4d97591a8974e808e70c112907
Momentao <91534139+Momentao@users.noreply.github.com>
2023-02-09 22:09:40 +0000

    Edit

--- next.go
+++ next.go
@@ -2,4 +2,6 @@ package main
 
 // This file implements the "next" command
 
-//func (player *Player) Next(args []
+//func (player *Player) Next(args []string) (resp, err string) {
+//	if len(args) > 0 {
+//		er
