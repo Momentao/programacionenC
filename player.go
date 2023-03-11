c7744f4a2c0eb9658de9156059a2eaf61ded8691
Momentao <91534139+Momentao@users.noreply.github.com>
2023-03-11 18:36:16 +0000

    :arrow_up: Upgrade gems

--- player.go
+++ player.go
@@ -2,4 +2,7 @@ package main
 
 // This file implements the player state.
 // The player communicates over RPC (Remote Procedure Call)
-// with the client. The client forks a player daemon (cli
+// with the client. The client forks a player daemon (clip -d)
+// if none is yet running and sends RPC calls to it.
+
+impo
