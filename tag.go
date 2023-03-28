c35bfa0b4e40acc13f67436e359609f7e436a904
Momentao <91534139+Momentao@users.noreply.github.com>
2023-03-28 07:35:33 +0000

    First Checked In.

--- tag.go
+++ tag.go
@@ -2,4 +2,5 @@ package main
 
 // The tag tree is used to look up music clips
 // using fuzzy matching (so that the name does not
-// have to be typed 100% correct on the co
+// have to be typed 100% correct on the command line)
+// A tag may represent, e.g., an ar
