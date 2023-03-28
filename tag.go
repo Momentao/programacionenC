1e52c985a638c5dd2f2f1983d28bc5ee3ad3c948
Momentao <91534139+Momentao@users.noreply.github.com>
2023-03-28 08:11:47 +0000

    pr fixes

--- tag.go
+++ tag.go
@@ -3,4 +3,5 @@ package main
 // The tag tree is used to look up music clips
 // using fuzzy matching (so that the name does not
 // have to be typed 100% correct on the command line)
-// A tag may represent, e.g., an ar
+// A tag may represent, e.g., an artist, album, clip...
+// Some tags, like albums, have child Tag
